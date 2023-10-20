b2d9b678f3d757b189b7156ca18f06e7a90a9808
Paresh Barge <89344241+Paresh15@users.noreply.github.com>
2023-10-20 20:05:18 +0530

    render backend

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,27 @@ import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Context } from "./context/Context";
+import axios from "axios";
+
+const getData = async () => {
+  try {
+    const res = await axios.get("/");
+    console.log(res)
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 function App() {
   const { user } = useContext(Context);
+
+  useEffect(() => {
+    console.log("fetching data")
+    getData();
+  }, [])
+
   return (
     <Router>
       <TopBar />
